6f0d8d0c5575a8102f3cba28b005d1345111a64e
Naima Hassan <naima.hassan434@gmail.com>
2022-06-05 19:30:49 +0100

    Bug fix 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 
@@ -9,8 +9,9 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
+  const mounted = useRef(false);
 
   const fetchImages = async () => {
     setLoading(true);
@@ -46,24 +47,39 @@ function App() {
 
   useEffect(() => {
     fetchImages();
+    // eslint-disable-next-line
   }, [page]);
 
   useEffect(() => {
-    const event = window.addEventListener("scroll", () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return oldPage + 1;
-        });
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    console.log("second");
   }, []);
+
+  // useEffect(() => {
+  //   const event = window.addEventListener("scroll", () => {
+  //     if (
+  //       !loading &&
+  //       window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+  //     ) {
+  //       setPage((oldPage) => {
+  //         return oldPage + 1;
+  //       });
+  //     }
+  //   });
+  //   return () => window.removeEventListener("scroll", event);
+  //   // eslint-disable-next-line
+  // }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+      return;
+    }
     setPage(1);
   };
   return (
